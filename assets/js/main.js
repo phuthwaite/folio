df25d611135d2adc09c713695d3a86d234c7bca8
phuthwaite <hello@pog.gs>
2023-06-22 16:16:33 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -282,7 +282,7 @@ $(document).ready(function(){
     var arr = ['assets/img/bg/13_insta@_poggs.jpg', 'assets/img/bg/canarywharf_insta@_poggs.jpg', 'assets/img/bg/gourock_insta@_poggs.jpg', 'assets/img/bg/londonbridge_insta@_poggs.jpg', 'assets/img/bg/myrkdalen_insta@_poggs.jpg', 'assets/img/bg/palmsprings_insta@_poggs.jpg', 'assets/img/bg/takethat_insta@_poggs.jpg'];
     arr = shuffle(arr);
     // console.log("1st of arr is " + arr[1]);
-    document.getElementById('home-slider').style.backgroundImage = "url('" + arr[1] + "')";
+    document.getElementById('home-slider').backgroundImage = "url('" + arr[1] + "')";
 });
 
 $(document).on("scroll", function(){
